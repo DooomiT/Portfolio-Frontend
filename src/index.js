2da345eed975366556bf6d0317cd919d086ae3c1
Florian.Bufler <Florian.Bufler@dhbw-engineering.de>
2021-05-14 13:32:41 +0200

    Implemented Some Basic Structure

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/images')); //
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    express.static(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
